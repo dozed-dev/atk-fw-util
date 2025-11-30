49f927f938bcb09964f22e2456d5c3cd5e8823be
dozed-dev <dev.dozed@aleeas.com>
2025-12-01 00:07:14 +0300

    fix FirmwareHeader, add pack

--- src/atk_fw_util/FirmwareHeader.py
+++ src/atk_fw_util/FirmwareHeader.py
@@ -1,6 +1,5 @@
 import struct
-from dataclasses import dataclass
-from .defs import HEADER_FORMAT
+from dataclasses import dataclass, astuple
 
 @dataclass
 class FirmwareHeader:
@@ -15,8 +14,13 @@ class FirmwareHeader:
     month: int
     day: int
 
+HEADER_FORMAT = "<16s B B H H H I H B B"
+
 def parse_header(data: bytes) -> FirmwareHeader:
     fields = struct.unpack(HEADER_FORMAT, data[:struct.calcsize(HEADER_FORMAT)])
     name = fields[0].split(b"\x00", 1)[0].decode("ascii", errors="ignore")
     return FirmwareHeader(name, *fields[1:])
 
+def pack_header(header: FirmwareHeader) -> bytes:
+    s = header.name.encode('utf-8')
+    return struct.pack(HEADER_FORMAT, s, *(astuple(header)[1:]))
--- src/atk_fw_util/pack.py
+++ src/atk_fw_util/pack.py
@@ -1,2 +1,19 @@
-def pack(input, output):
-    pass
+from .defs import *
+from .FirmwareHeader import FirmwareHeader, pack_header
+from shutil import copyfileobj
+
+def pack(in_stream, out_stream):
+    cur_pos = in_stream.tell()
+    size = in_stream.seek(0, 2) - cur_pos
+    in_stream.seek(cur_pos)
+
+    header = FirmwareHeader(name="ATK-PTT80P",
+                            fw_size=size,
+                            encrypt_pos=0xff, # this encrypt pos bypasses encryption completely
+                            crc=0, # crc is 0 for now
+                            data_len=CHUNK_SIZE,
+                            app_addr=1, app_ver=1,  
+                            year=2025, month=11, day=30)
+
+    out_stream.write(pack_header(header))
+    copyfileobj(in_stream, out_stream)
